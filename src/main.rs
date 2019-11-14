df91902524c849ea5018acf7051e54173ff75dbc
grammers <samkar-4@student.ltu.se>
2019-11-14 12:14:17 +0100

    vorks vith v5 stm32-usbd local dependesi

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,21 @@ use cortex_m_semihosting::hprintln;
 use cortex_m::asm::delay;
 use rtfm::app;
 
-use hal::{prelude::*, stm32, hal::digital::v2::OutputPin, gpio::{Output, PushPull}};
-
-use stm32_usbd::{UsbBus, UsbBusType};
-use usb_device::{bus, prelude::*};
+use hal::{prelude::*, stm32, hal::digital::v2::OutputPin,
+	usb::{Peripheral, UsbBus, UsbBusType},
+	gpio::{Output, PushPull}};
+use usb_device::{prelude::*, bus, bus::UsbBus as UB};
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-fn configure_usb_clock() {
-    let rcc = unsafe { &*stm32::RCC::ptr() };
-    rcc.cfgr.modify(|_, w| w.usbpre().set_bit());
-}
 
 #[app(device = stm32f3xx_hal::stm32)]
 const APP: () = {
 
 	static mut LED_S: hal::gpio::gpioe::PE13<Output<PushPull>> = ();
     static mut USB_DEV: UsbDevice<'static, UsbBusType> = ();
-    static mut SERIAL: SerialPort<'static, UsbBusType> = ();
+	static mut SERIAL: SerialPort<'static, UsbBusType> = ();
 
+	
 	#[init]
 	fn init() {
   
@@ -37,6 +34,7 @@ const APP: () = {
         let mut rcc = device.RCC.constrain();
         let clocks = rcc
 			.cfgr
+			.use_hse(8.mhz())
             .sysclk(48.mhz())
             .pclk1(24.mhz())
 			.pclk2(24.mhz())
@@ -44,23 +42,31 @@ const APP: () = {
 
 		// LEDs
 		let mut gpioe = device.GPIOE.split(&mut rcc.ahb);
-		let led_s = gpioe.pe13.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+		let led_s = gpioe.pe13
+			.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 		
 		// USB pin set upp
 		let mut gpioa = device.GPIOA.split(&mut rcc.ahb);
         // BluePill board has a pull-up resistor on the D+ line.
         // Pull the D+ pin down to send a RESET condition to the USB bus.
-		let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+		let mut usb_dp = gpioa.pa12
+			.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 		usb_dp.set_low().expect("could not set pin for synk");
 		delay(clocks.sysclk().0 / 100);
 
-		let usb_dm = gpioa.pa11.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
-		let usb_dp = usb_dp.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
+		let usb_dm = gpioa.pa11
+			.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
+		let usb_dp = usb_dp
+			.into_af14(&mut gpioa.moder, &mut gpioa.afrh);
 
-		configure_usb_clock();
+		let usb = Peripheral {
+			usb: device.USB,
+			pin_dm: usb_dm,
+			pin_dp: usb_dp,
+		};
 
 		// USB bus, serial conection and device set up
-        *USB_BUS = Some(UsbBus::new(device.USB, (usb_dm, usb_dp)));
+        *USB_BUS = Some(UsbBus::new(usb));
         let serial = SerialPort::new(USB_BUS.as_ref().unwrap());
        let usb_dev =
             UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
@@ -73,9 +79,8 @@ const APP: () = {
 		LED_S = led_s;
 		USB_DEV = usb_dev;
 		SERIAL = serial;
-
 	}
-
+	
     #[interrupt(resources = [USB_DEV, SERIAL])]
     fn USB_HP_CAN_TX() {
         usb_poll(&mut resources.USB_DEV, &mut resources.SERIAL);
@@ -92,7 +97,7 @@ const APP: () = {
 };
 
 
-fn usb_poll<B: bus::UsbBus>(
+fn usb_poll<B: UB>(
     usb_dev: &mut UsbDevice<'static, B>,
     serial: &mut SerialPort<'static, B>,
 ) {
@@ -121,3 +126,4 @@ fn usb_poll<B: bus::UsbBus>(
         _ => {}
     }
 }
+
